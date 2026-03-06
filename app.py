c100e523b45270d81a6cdd593ec15d522d1827ed
Slovian-nss <gremijum.slovjanskogo@gmail.com>
2026-03-06 01:29:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -203,7 +203,7 @@ Zwróć TYLKO czyste tłumaczenie używając alfabetu łacińskiego + ě, ę, ǫ
                     {"role": "system", "content": system_prompt},
                     {"role": "user", "content": f"BAZA:\n{context_str}\n\nDO TŁUMACZENIA: {user_input}"}
                 ],
-                model="groq/compound",
+                model="llama-3.1-8b-instant",
                 temperature=0.0
             )
 
@@ -219,5 +219,6 @@ Zwróć TYLKO czyste tłumaczenie używając alfabetu łacińskiego + ě, ę, ǫ
 
         except Exception as e:
             st.error(f"Blǫd umětьnogo uma: {e}")
+
 
 
