115d4d9d162587cbc02b74744b567d91378d377c
Slovian-nss <gremijum.slovjanskogo@gmail.com>
2026-03-23 21:13:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def index():
     return render_template('index.html', original=original, result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+  app.run(host='0.0.0.0', port=8080, debug=False)
